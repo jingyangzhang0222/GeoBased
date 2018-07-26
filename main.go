d8fc8c1c175dcf7a7ea037cfcfaa858ade603b05
Jingyang Zhang <jingyangzhang@172-16-43-53.dynapool.nyu.edu>
2018-07-26 14:39:31 -0400

    0726

--- main.go
+++ main.go
@@ -9,8 +9,9 @@ import (
 	"reflect"
 	"strconv"
 	"github.com/pborman/uuid"
-	"cloud.google.com/go/bigtable"
 	"context"
+	"io"
+	"cloud.google.com/go/storage"
 )
 
 const (
@@ -22,6 +23,9 @@ const (
 	BT_INSTANCE = "around-post"
 	// Needs to update this URL if you deploy it to cloud.
 	ES_URL = "http://35.196.231.235:9200"
+
+	BUCKET_NAME = "post-images-210816"
+
 )
 
 // 1.3.1 Encode json object
@@ -41,6 +45,8 @@ type Post struct {
 	User     string   `json:"user"`
 	Message  string   `json:"message"`
 	Location Location `json:"location"`
+
+	Url    string `json:"url"`
 }
 
 func main() {
@@ -95,6 +101,60 @@ func main() {
 }
 
 func handlerPost(w http.ResponseWriter, r *http.Request) {
+	/*	2.0 Google Cloud storage*/
+	w.Header().Set("Content-Type", "application/json")
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+	w.Header().Set("Access-Control-Allow-Headers", "Content-Type,Authorization")
+
+
+	// 32 << 20 is the maxMemory param for ParseMultipartForm, equals to 32MB (1MB = 1024 * 1024 bytes = 2^20 bytes)
+	// After you call ParseMultipartForm, the file will be saved in the server memory with maxMemory size.
+	// If the file size is larger than maxMemory, the rest of the data will be saved in a system temporary file.
+	r.ParseMultipartForm(32 << 20)
+
+	// Parse from form data.
+	fmt.Printf("Received one post request %s\n", r.FormValue("message"))
+	lat, _ := strconv.ParseFloat(r.FormValue("lat"), 64)
+	lon, _ := strconv.ParseFloat(r.FormValue("lon"), 64)
+	p := &Post{
+		User:    "1111",
+		Message: r.FormValue("message"),
+		Location: Location{
+			Lat: lat,
+			Lon: lon,
+		},
+	}
+
+	id := uuid.New()
+
+	file, _, err := r.FormFile("image")
+	if err != nil {
+		http.Error(w, "Image is not available", http.StatusInternalServerError)
+		fmt.Printf("Image is not available %v.\n", err)
+		return
+	}
+	defer file.Close()
+
+	ctx := context.Background()
+
+	// replace it with your real bucket name.
+	_, attrs, err := saveToGCS(ctx, file, BUCKET_NAME, id)
+	if err != nil {
+		http.Error(w, "GCS is not setup", http.StatusInternalServerError)
+		fmt.Printf("GCS is not setup %v\n", err)
+		return
+	}
+
+	// Update the media link after saving to GCS.
+	p.Url = attrs.MediaLink
+
+	// Save to ES.
+	saveToES(p, id)
+
+	// Save to BigTable.
+	//saveToBigTable(p, id)
+
+	/* 1.0
 	// Parse from body of request to get a json object.
 	fmt.Println("Reveived a Post Request")
 	var p Post
@@ -120,7 +180,6 @@ func handlerPost(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-
 	tbl := bt_client.Open("post")
 	mut := bigtable.NewMutation()
 	t := bigtable.Now()
@@ -138,7 +197,47 @@ func handlerPost(w http.ResponseWriter, r *http.Request) {
 
 	fmt.Printf("Post is saved to BigTable: %s\n", p.Message)
 	fmt.Fprintf(w, "Post Received: %s\n", p.Message)
+	*/
 }
+
+func saveToGCS(ctx context.Context, r io.Reader, bucket, name string) (*storage.ObjectHandle, *storage.ObjectAttrs, error) {
+	/*
+	Implement saveToGCS. Google has provided a good example of writing objects to GCS.
+	https://cloud.google.com/storage/docs/reference/libraries#client-libraries-install-go
+
+	Google example of writing an object to GCS (copied from
+	https://github.com/GoogleCloudPlatform/golang-samples/blob/master/storage/objects/main.go)
+	*/
+	client, err := storage.NewClient(ctx)
+	if err != nil {
+		return nil, nil, err
+	}
+	defer client.Close()
+
+	bh := client.Bucket(bucket)
+	if _, err = bh.Attrs(ctx); err != nil {
+		return nil, nil, err
+	}
+
+	obj := bh.Object(name) // name: uuid
+	wc := obj.NewWriter(ctx)
+	if _, err = io.Copy(wc, r); err != nil {
+		return nil, nil, err
+	}
+	if err := wc.Close(); err != nil {
+		return nil, nil, err
+	}
+
+	// access control list
+	if err := obj.ACL().Set(ctx, storage.AllUsers, storage.RoleReader); err != nil {
+		return nil, nil, err
+	}
+
+	attrs, err := obj.Attrs(ctx)
+	fmt.Printf("Post is saved to GCS: %s\n", attrs.MediaLink)
+	return obj, attrs, err
+}
+
 
 // 1.3.6
 func handlerSearch(w http.ResponseWriter, r *http.Request) {
